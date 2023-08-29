8b5d7f50a2538446a7f4ecbff8be7607bf47ca88
Artem Khrapuzov <tema102@mail.ru>
2023-08-29 16:55:19 +0600

    create serializer with class serializer.Serializer with def create, def update. Create class (APIView) in views.py with def get, post, put, delete

--- women/serializer.py
+++ women/serializer.py
@@ -1,10 +1,48 @@
+import io
+
 from rest_framework import serializers
+from rest_framework.parsers import JSONParser
+from rest_framework.renderers import JSONRenderer
 
 from .models import Women
 
 
+# class WomenModel: # класс - объекты которого мы будем реализовать(преобразовывать в json строку)
+#     def __init__(self, title, content):
+#         self.title = title
+#         self.content = content
 
-class WomenSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Women
-        fields = ('title', 'cat_id')
+class WomenSerializer(serializers.ModelSerializer):  # функционал для преобразования объектов класса WomenModel в json
+    title = serializers.CharField(max_length=255)
+    content = serializers.CharField()
+    time_create = serializers.DateTimeField(read_only=True)
+    time_update = serializers.DateTimeField(read_only=True)
+    is_published = serializers.BooleanField(default=True)
+    cat_id = serializers.IntegerField()
+
+    def create(self, validated_data):
+        return Women.objects.create(**validated_data)
+
+    def update(self, instance, validated_data):
+        instance.title = validated_data.get("title", instance.title)
+        instance.content = validated_data.get("content", instance.content)
+        instance.time_update = validated_data.get("time_update", instance.time_update)
+        instance.is_published = validated_data.get("is_published", instance.is_published)
+        instance.cat_id = validated_data.get("cat_id", instance.cat_id)
+        instance.save()
+        return instance
+
+# def encode(): #Функция кодирующая в json обьекты из класса WomenModel
+#     model = WomenModel('Angelina Jolie', 'Content: Angelina Jolie')
+#     model_sr = WomenSerializer(model) # словарь
+#     print(model_sr.data)
+#     json = JSONRenderer().render(model_sr.data) # байтовая строка в json формате
+#     print(json)
+#
+#
+# def decode(): # из json в объекты класса WomenModel
+#     stream = io.BytesIO(b'{"title": "Angelina Jolie", "content":"Content: Angelina Jolie"}')
+#     data = JSONParser().parse(stream)
+#     serializer = WomenSerializer(data=data)
+#     serializer.is_valid()
+#     print(serializer.validated_data)
